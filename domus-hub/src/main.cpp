635c50f702d00ee1cc6eda9c9e4de0ea093c5c8a
Matteo <matteo.violani@studio.unibo.it>
2023-01-17 22:56:31 +0100

    feat(main): added to scheduling recv and send homeState

--- domus-hub/src/main.cpp
+++ domus-hub/src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
 
+#include "HomeState.h"
 #include "Scheduler.h"
 #include "./Tasks/GarageTask.h"
 #include "./Tasks/DisplayTask.h"
-#include "boundary/garage-door/GarageDoor.h"
 #include "./Tasks/IndoorLightTask.h"
-#include "HomeState.h"
+#include "./Tasks/Communication/SendDataTask.h"
+#include "./Tasks/Communication/RecvDataTask.h"
 
 #define SCHED_PERIOD 10
 
@@ -14,22 +15,34 @@ Scheduler sched;
 GarageTask* garageTask;
 DisplayTask* displayTask;
 IndoorLightTask* indoorLightTask;
+SendDataTask* sendDataTask;
+RecvDataTask* recvDataTask;
 
 void setup() {
     Serial.begin(9600);
     sched.init(SCHED_PERIOD);
     
     garageTask = new GarageTask(&homeState);
-    garageTask->init(100);
+    garageTask->init(60);
     sched.addTask(garageTask);
 
     displayTask = new DisplayTask(&homeState);
-    displayTask->init(500);
+    displayTask->init(800);
     sched.addTask(displayTask);
+    //displayTask->disable();
 
     indoorLightTask = new IndoorLightTask(&homeState);
-    indoorLightTask->init(250);
+    indoorLightTask->init(300);
     sched.addTask(indoorLightTask);
+
+    sendDataTask = new SendDataTask(&homeState);
+    sendDataTask->init(1500);
+    sched.addTask(sendDataTask);
+
+    recvDataTask = new RecvDataTask(&homeState);
+    recvDataTask->init(30);
+    sched.addTask(recvDataTask);
+
 }   
 
 void loop() {
