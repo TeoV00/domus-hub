e3105134cf8b9813f2a0be7856b5f26c6063db76
Matteo <matteo.violani@studio.unibo.it>
2023-01-19 17:28:26 +0100

    fix(SendDataTask): fixed indentation

--- domus-hub/src/Tasks/Communication/SendDataTask.cpp
+++ domus-hub/src/Tasks/Communication/SendDataTask.cpp
@@ -8,27 +8,25 @@ SendDataTask::SendDataTask(HomeState* homeState) {
 };
 
 void SendDataTask::init(int timeoutExec){
-  Task::init(timeoutExec);
-  Serial.begin(9600);
+    Task::init(timeoutExec);
 }
 
 void SendDataTask::tick() {
-  Serial.print("{\"inLight\":");
-  Serial.print(this->homeState->inLight);
-  Serial.print(",\"outLight\":");
-  Serial.print(this->homeState->outLight);
-  Serial.print(",\"alarmLight\":");
-  Serial.print(this->homeState->alarmLight);
-  Serial.print(",\"alarmPwr\":");
-  Serial.print(this->homeState->alarmPwr);
-  Serial.print(",\"moveDetected\":");
-  Serial.print(this->homeState->moveDetected);
-  Serial.print(",\"heatSysPwr\":");
-  Serial.print(this->homeState->heatSysPwr);
-  Serial.print(",\"heatTemp\":");
-  Serial.print(this->homeState->heatTemp);
-  Serial.print(",\"garageState\":");
-  Serial.print(this->homeState->garageState);
-  Serial.println("}");
-
+    Serial.print("{\"inLight\":");
+    Serial.print(this->homeState->inLight);
+    Serial.print(",\"outLight\":");
+    Serial.print(this->homeState->outLight);
+    Serial.print(",\"alarmLight\":");
+    Serial.print(this->homeState->alarmLight);
+    Serial.print(",\"alarmPwr\":");
+    Serial.print(this->homeState->alarmPwr);
+    Serial.print(",\"moveDetected\":");
+    Serial.print(this->homeState->moveDetected);
+    Serial.print(",\"heatSysPwr\":");
+    Serial.print(this->homeState->heatSysPwr);
+    Serial.print(",\"heatTemp\":");
+    Serial.print(this->homeState->heatTemp);
+    Serial.print(",\"garageState\":");
+    Serial.print(this->homeState->garageState);
+    Serial.println("}");
 };
