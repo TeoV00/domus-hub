168ce1ae4c3d390294c8fec6cf7cfdde5cf60639
Matteo <matteo.violani@studio.unibo.it>
2023-01-19 13:18:58 +0100

    feat(InputValues): added struct to keep track of inputs value readed

---
+++ domus-hub/include/InputValues.h
@@ -0,0 +1,11 @@
+#ifndef __INPUT_VALUES__
+#define __INPUT_VALUES__
+
+struct InputValues {
+    bool btnAlarm = false;
+    bool btnHeat = false;
+    bool motionSensor = false;
+    int tempHeatSelector = 0;
+};
+
+#endif
